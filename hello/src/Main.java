e9b625c7c4ae76e8ec3afa3f7ce9751c1777e6a2
hjchu <3263518006@qq.com>
2024-03-25 18:10:49 +0800

    first commit

---
+++ hello/src/Main.java
@@ -0,0 +1,17 @@
+
+/**
+ * Learn Java from https://www.liaoxuefeng.com/
+ * 
+ * @author liaoxuefeng
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");
+	}
+	public static String print(String s) {
+		
+		return s;
+	}
+
+}
---
+++ hello/test/MainTest.java
@@ -0,0 +1,15 @@
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+	@Test
+	void testFact() {
+		assertEquals("hello",Main.print("hello"));
+	}
+
+}
